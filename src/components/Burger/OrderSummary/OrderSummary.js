aee4c5e098980b9dbd80561cd26aeb4278364df6
Antoine <antoinepoppe@hotmail.fr>
2021-02-03 11:48:54 +0100

    Improve pref - Add modal update verif

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../Ui/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredients = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    componentWillUpdate()  {
+        console.log("update")
+    }
+
+    render () {
+        const ingredients = Object.keys(this.props.ingredients)
         .map((igKey, index) => {
             return (
             <li key={index}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]} 
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]} 
             </li> 
             //style {{}} pour annoncer du contenu dynamique et pour js object
             ) // paranthese parce que return sur plusieurs lignes
         });
 
-    return (
-        <Aux>
-            <h3>Your order</h3>
-            <p>A burger with the following ingredients:</p>
-            <ul>
-                {ingredients}
-            </ul>
-            <p>Total Price: {props.totalPrice.toFixed(2)}</p>
-            <p>Continue to checkout ?</p>
-            <Button clicked={props.cancelPurchase} btnType="Danger">CANCEL</Button>
-            <Button clicked={props.continuePurchase} btnType="Success">CONTINUE</Button>
-        </Aux>
-    )
-};
+        return (
+            <Aux>
+                <h3>Your order</h3>
+                <p>A burger with the following ingredients:</p>
+                <ul>
+                    {ingredients}
+                </ul>
+                <p>Total Price: {this.props.totalPrice.toFixed(2)}</p>
+                <p>Continue to checkout ?</p>
+                <Button clicked={this.props.cancelPurchase} btnType="Danger">CANCEL</Button>
+                <Button clicked={this.props.continuePurchase} btnType="Success">CONTINUE</Button>
+            </Aux>
+        )
+    }
 
-export default orderSummary;
+}
+    
+
+export default OrderSummary;
